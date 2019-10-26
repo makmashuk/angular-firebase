980b4d58f1456070b6a91051997f59664e5827bc
makmashuk <makmashuk@gmail.com>
2019-10-26 23:16:32 +0600

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDJkWffaZz_NyU_GNfMbGhEGCW7LAwSOkc",
+    authDomain: "fileupload-ecbe0.firebaseapp.com",
+    databaseURL: "https://fileupload-ecbe0.firebaseio.com",
+    projectId: "fileupload-ecbe0",
+    storageBucket: "fileupload-ecbe0.appspot.com",
+    messagingSenderId: "240167001454",
+    appId: "1:240167001454:web:133b1e53840acf7db9b57f"
+  }
+
 };
 
 /*
